b28d67e3bee712d1d61a5350788d1df48432c58b
Pratham Gupta <prathamgupta@Embibes-MacBook-Air-2.local>
2021-02-23 18:02:20 +0530

    vdjhb

--- bmi-test.py
+++ bmi-test.py
@@ -1,8 +1,54 @@
 from unittest import TestCase
+from bmi import calculate_bmi
 
-class TestUnderWeight(TestCase):
-    def test_under_weight_success(self):
-        """
+dictionary_people_list_height = [{'Gender': 'Male', 'HeightCm': -171, 'WeightKg': 40},
+                                 {'Gender': 'Male', 'HeightCm': 161, 'WeightKg': 30},
+                                 {'Gender': 'Male', 'HeightCm': 180, 'WeightKg': 22},
+                                 {'Gender': 'Female', 'HeightCm': 166, 'WeightKg': 22},
+                                 {'Gender': 'Female', 'HeightCm': 150, 'WeightKg': 40},
+                                 {'Gender': 'Female', 'HeightCm': 167, 'WeightKg': 33}
+                                 ]
+dictionary_people_list_weight = [{'Gender': 'Male', 'HeightCm': 171, 'WeightKg': 40},
+                                 {'Gender': 'Male', 'HeightCm': 161, 'WeightKg': -30},
+                                 {'Gender': 'Male', 'HeightCm': 180, 'WeightKg': 22},
+                                 {'Gender': 'Female', 'HeightCm': 166, 'WeightKg': 22},
+                                 {'Gender': 'Female', 'HeightCm': 150, 'WeightKg': 40},
+                                 {'Gender': 'Female', 'HeightCm': 167, 'WeightKg': 33}
+                                 ]
+dictionary_people_list_heaviest_weight = [{'Gender': 'Male', 'HeightCm': 171, 'WeightKg': 40},
+                                          {'Gender': 'Male', 'HeightCm': 161, 'WeightKg': 444},
+                                          {'Gender': 'Male', 'HeightCm': 180, 'WeightKg': 22},
+                                          {'Gender': 'Female', 'HeightCm': 166, 'WeightKg': 22},
+                                          {'Gender': 'Female', 'HeightCm': 150, 'WeightKg': 40},
+                                          {'Gender': 'Female', 'HeightCm': 167, 'WeightKg': 33}
+                                          ]
 
-        :return: True or False
-        """
+dictionary_people_list_tallest = [{'Gender': 'Male', 'HeightCm': 274, 'WeightKg': 40},
+                                  {'Gender': 'Male', 'HeightCm': 161, 'WeightKg': 44},
+                                  {'Gender': 'Male', 'HeightCm': 180, 'WeightKg': 22},
+                                  {'Gender': 'Female', 'HeightCm': 166, 'WeightKg': 22},
+                                  {'Gender': 'Female', 'HeightCm': 150, 'WeightKg': 40},
+                                  {'Gender': 'Female', 'HeightCm': 167, 'WeightKg': 33}
+                                  ]
+
+
+class TestOverWeight(TestCase):
+    def test_for_less_than_zero_height(self):
+        actual = calculate_bmi(dictionary_people_list=dictionary_people_list_height)
+        expected = "Please enter a valid height"
+        self.assertEqual(actual, expected, "Failed for validation for height")
+
+    def test_for_less_than_zero_weight(self):
+        actual = calculate_bmi(dictionary_people_list=dictionary_people_list_weight)
+        expected = "Please enter a valid weight"
+        self.assertEqual(actual, expected, "Failed for validation for weight")
+
+    def test_for_less_than_heaviest_weight(self):
+        actual = calculate_bmi(dictionary_people_list=dictionary_people_list_heaviest_weight)
+        expected = "Please enter a valid weight"
+        self.assertEqual(actual, expected, "Failed for validation for weight")
+
+    def test_for_the_tallest(self):
+        actual = calculate_bmi(dictionary_people_list=dictionary_people_list_tallest)
+        expected = "Please enter a valid height"
+        self.assertEqual(actual, expected, "Failed for validation for height")
--- bmi.py
+++ bmi.py
@@ -1,49 +1,68 @@
-dictionary_people_list = [{"Gender": "Male", "HeightCm": 171, "WeightKg": 96},
-                          {"Gender": "Male", "HeightCm": 161, "WeightKg": 85},
-                          {"Gender": "Male", "HeightCm": 180, "WeightKg": 77},
-                          {"Gender": "Female", "HeightCm": 166, "WeightKg": 62},
-                          {"Gender": "Female", "HeightCm": 150, "WeightKg": 70},
-                          {"Gender": "Female", "HeightCm": 167, "WeightKg": 82}
-                          ]
+# dictionary_people_list = [{'Gender': 'Male', 'HeightCm': 171, 'WeightKg': 96},
+#                           {'Gender': 'Male', 'HeightCm': 161, 'WeightKg': 85},
+#                           {'Gender': 'Male', 'HeightCm': 180, 'WeightKg': 77},
+#                           {'Gender': 'Female', 'HeightCm': 166, 'WeightKg': 62},
+#                           {'Gender': 'Female', 'HeightCm': 150, 'WeightKg': 70},
+#                           {'Gender': 'Female', 'HeightCm': 167, 'WeightKg': 82}
+#                           ]
+# dictionary_people_list = [{'Gender': 'Male', 'HeightCm': 171, 'WeightKg': 40},
+#                           {'Gender': 'Male', 'HeightCm': 161, 'WeightKg': 30},
+#                           {'Gender': 'Male', 'HeightCm': 180, 'WeightKg': 22},
+#                           {'Gender': 'Female', 'HeightCm': 166, 'WeightKg': 22},
+#                           {'Gender': 'Female', 'HeightCm': 150, 'WeightKg': 40},
+#                           {'Gender': 'Female', 'HeightCm': 167, 'WeightKg': 33}
+#                           ]
 
 
 def calculate_bmi(dictionary_people_list):
+    new_list = []
     for people in dictionary_people_list:
         new_dict = {}
-        convert_into_mt = people["HeightCm"] / 100
-        bmi_index = people["WeightKg"] / convert_into_mt
-        new_dict["BMIIndex"] = round(bmi_index, 2)
+        if people['HeightCm'] <= 0:
+            return "Please enter a valid height"
+        if people['WeightKg'] <= 0:
+            return "Please enter a valid weight"
+        # Considering world's tallest man height ie 273 cm
+        if people["HeightCm"] >= 273:
+            return "Please enter a valid height"
+        # Considering world's heaviest man weight ie 442 kg
+        if people["WeightKg"] >= 442:
+            return "Please enter a valid weight"
+        convert_into_mt = people['HeightCm'] / 100
+        bmi_index = people['WeightKg'] / convert_into_mt
+        new_dict['BMIIndex'] = round(bmi_index, 2)
         if bmi_index < 18.5:
-            new_dict["BMICategory"] = "underweight"
-            new_dict["HealthRisk"] = "malnutrition risk"
+            new_dict['BMICategory'] = 'Underweight'
+            new_dict['HealthRisk'] = 'Malnutrition Risk'
         elif 18.5 <= bmi_index < 25:
-            new_dict["BMICategory"] = "normal weight"
-            new_dict["HealthRisk"] = "low risk"
+            new_dict['BMICategory'] = 'Normal Weight'
+            new_dict['HealthRisk'] = 'Low Risk'
         elif 25 <= bmi_index < 30:
-            new_dict["BMICategory"] = "overweight"
-            new_dict["HealthRisk"] = "enhanced risk"
+            new_dict['BMICategory'] = 'Overweight'
+            new_dict['HealthRisk'] = 'Enhanced Risk'
         elif 30 <= bmi_index < 35:
-            new_dict["BMICategory"] = "moderately obese"
-            new_dict["HealthRisk"] = "medium risk"
+            new_dict['BMICategory'] = 'Moderately Obese'
+            new_dict['HealthRisk'] = 'Medium Risk'
         elif 35 <= bmi_index < 40:
-            new_dict["BMICategory"] = "severely obese"
-            new_dict["HealthRisk"] = "high risk"
+            new_dict['BMICategory'] = 'Severely Obese'
+            new_dict['HealthRisk'] = 'High Risk'
         elif bmi_index >= 40:
-            new_dict["BMICategory"] = "very severely obese"
-            new_dict["HealthRisk"] = "very high risk"
-        people.update(new_dict)
+            new_dict['BMICategory'] = 'Very Severely Obese'
+            new_dict['HealthRisk'] = 'Very High Risk'
+        new_list.append(new_dict)
+    for i in range(len(dictionary_people_list)):
+        dictionary_people_list[i].update(new_list[i])
 
     return dictionary_people_list
 
 
 def count_of_overweight_people(dictionary_people_list):
     dictionary_people = calculate_bmi(dictionary_people_list)
-    print(dictionary_people)
     count = 0
     for people in dictionary_people:
-        if 25 <= people["BMIIndex"] < 30:
+        if 25 <= people['BMIIndex'] < 30:
             count = count + 1
     return count
 
-
-print(count_of_overweight_people(dictionary_people_list))
+# print(count_of_overweight_people(dictionary_people_list))
+# calculate_bmi(dictionary_people_list)
